9c01a980bcbc5060102ae87893b0f026595e1d91
Baker <dragonmost@live.ca>
2015-12-01 14:45:42 -0500

    python scores added to BD

--- parser.py
+++ parser.py
@@ -59,65 +59,61 @@ def BDStanding(team, pct, pf, pa):
 
 
 
-def ParseSchedule():
+def ParseScore():
     bd = sqlite3.connect("NFL.db")
 
-    bd.execute("drop table if exists schedule")
+    bd.execute("drop table if exists scores")
     bd.commit()
 
-    bd.execute('CREATE TABLE schedule(team1, team2, score1, score2)')
+    bd.execute('CREATE TABLE scores(score)')
     bd.commit()
 
 
     for i in range(1,17+1):
         response = urllib.request.urlopen('http://espn.go.com/nfl/schedule/_/week/' + str(i))
-        print('http://espn.go.com/nfl/schedule /_/week/' + str(i))
+        print('http://espn.go.com/nfl/schedule/_/week/' + str(i))
         html = response.read()
         response.close
 
         print(html)
 
 
-        test = False
+
         from bs4 import BeautifulSoup
         soup = BeautifulSoup(html,'html.parser')
 
-        #find tout les nom de team
-        links = soup.find_all('a',class_='team-name')
-        j = 0
-        for link in links:
-            names = link.contents[0]
-            #fulllink = link.get('')
+        rows = soup.find_all("tr")
+        for row in rows:
+            dataCol = 0
+            cols = row.find_all("td")
+            for col in cols:
+                a = col.find_all("a")
+                #print(a[0].contents[0])
+                if(dataCol == 2):
+                    #test = a[0].contents[0]
+                    #print(test)
+                    BDScore(a[0].contents[0])
+                if(dataCol == 5):
+                    dataCol = 0
+                else:
+                    dataCol = dataCol +1
+                '''
+                for team in a:
+                    names = team.contents[0]
+                    #print(team[0])
+                '''
 
-            if(names.string != None):
-                print(names.string)
-                #print(fulllink)
-
-                #find tout les scores pour chaque team
-                links2 = soup.findAll('tr')[j].find_all('td')
-                #BDSchedule(names, links2[4].contents[0], links2[9].contents[0], links2[10].contents[0])
-                for link2 in links2:
-                    stat = link2.contents[0]
-                    #print(link2)
-
-                    if(stat.string != None):
-                        print(stat.string)
-
-            #indice pour trouver le score associer avec la bonne team
-            #if test:
-            j = j + 1
-
-            #test = not(test)
-            print('\n')
+        print('\n')
 
     bd.close()
 
 
-def BDSchedule(team1, team2, score1, score2):
+def BDScore(score):
     bd = sqlite3.connect("NFL.db")
 
-    bd.execute("INSERT INTO schedule VALUES (?,?,?,?)",(team1, team2, score1, score2))
+    print(score)
+    bd.execute("INSERT INTO scores VALUES (?)",(score,))
     bd.commit()
     bd.close()
 
-ParseSchedule()
+ParseScore()
